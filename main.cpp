27560c17d0c50d72af260a4017b5d9f9fe60edba
aaaa24 <chernov.arseniy@yandex.ru>
2026-04-13 21:51:52 +0300

    created value

--- main.cpp
+++ main.cpp
@@ -14,7 +14,10 @@ struct BTreeIt {
 };
 
 template< class T, size_t K >
-T value(BTreeIt< T, K > it);
+T value(BTreeIt< T, K > it)
+{
+  return it.current->val[it.s];
+}
 
 template< class T, size_t K >
 BTreeIt< T, K > next(BTreeIt< T, K > it);
