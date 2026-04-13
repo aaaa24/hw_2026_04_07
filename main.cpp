d342ee019c8a3c480bb8d707bbdc5159d3d2df9a
aaaa24 <chernov.arseniy@yandex.ru>
2026-04-13 21:57:00 +0300

    add minimum and maximum

--- main.cpp
+++ main.cpp
@@ -14,13 +14,40 @@ struct BTreeIt {
 };
 
 template< class T, size_t K >
+BTree< T, K > * minimum(BTree< T, K > * root)
+{
+  if (!root) {
+    return root;
+  }
+  while (root->childs[0]) {
+    root = root->childs[0];
+  }
+  return root;
+}
+
+template< class T, size_t K >
+BTree< T, K > * maximum(BTree< T, K > * root)
+{
+  if (!root) {
+    return root;
+  }
+  while (root->childs[K]) {
+    root = root->childs[K];
+  }
+  return root;
+}
+
+template< class T, size_t K >
 T value(BTreeIt< T, K > it)
 {
   return it.current->val[it.s];
 }
 
 template< class T, size_t K >
-BTreeIt< T, K > next(BTreeIt< T, K > it);
+BTreeIt< T, K > next(BTreeIt< T, K > it)
+{
+
+}
 
 template< class T, size_t K >
 BTreeIt< T, K > prev(BTreeIt< T, K > it);
